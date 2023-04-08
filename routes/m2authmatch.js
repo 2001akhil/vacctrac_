8511043d92c7e04f55d78b3ade642a82b067a5c4
Akhil.A <56401501+2001akhil@users.noreply.github.com>
2023-04-08 17:06:58 +0000

    m2 auth have some error

--- routes/m2authmatch.js
+++ routes/m2authmatch.js
@@ -1,25 +1,25 @@
-var db = require("../dbconnector/connection");
+// var db = require("../dbconnector/connection");
 
-const checkMail = (email) => {
-  return new Promise((resolve, reject) => {
-    let sql = "select * from  where email = ?";
-    db.query(sql, [email], (err, result) => {
-      console.log(result[0]);
-      if (err) {
-        reject(err);
-      } else {
-        if (result.length > 0) {
-          resolve({
-            status: "SUCCESS",
-            data: result[0],
-            message: "Data fetched",
-          });
-        } else {
-          resolve({ status: "ERROR", data: null, message: "No data" });
-        }
-      }
-    });
-  });
-};
+// const checkMail = (email) => {
+//   return new Promise((resolve, reject) => {
+//     let sql = "select * from  where email = ?";
+//     db.query(sql, [email], (err, result) => {
+//       console.log(result[0]);
+//       if (err) {
+//         reject(err);
+//       } else {
+//         if (result.length > 0) {
+//           resolve({
+//             status: "SUCCESS",
+//             data: result[0],
+//             message: "Data fetched",
+//           });
+//         } else {
+//           resolve({ status: "ERROR", data: null, message: "No data" });
+//         }
+//       }
+//     });
+//   });
+// };
 
-module.exports = checkMail;
+// module.exports = checkMail;
