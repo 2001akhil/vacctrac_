c36858d193b1635956a33999822bbe6a50b5f7a8
Akhil.A <56401501+2001akhil@users.noreply.github.com>
2023-04-20 15:56:27 +0530

    data leakage cleared

--- dbverify/manufac.js
+++ dbverify/manufac.js
@@ -43,7 +43,8 @@ module.exports={
                   }
                   else{
                         resolve({
-                              
+                              //pending
+
 
                         })
                   }
