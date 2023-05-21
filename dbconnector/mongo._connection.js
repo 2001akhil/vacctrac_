79aefb62ae70fc6ab70e7d5d124b91eec0bdd85f
Akhil.A <56401501+2001akhil@users.noreply.github.com>
2023-05-22 00:35:41 +0530

    mongo connection added

---
+++ dbconnector/mongo._connection.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+//database name is vacctrac
+
+const url= "mongodb://localhost/vacctrac";
+
+
+mongoose
+  .connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+    
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
