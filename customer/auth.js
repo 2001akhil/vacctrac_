f690720a42926c8b08135cf0614e9c2eee3a16e5
Akhil.A <56401501+2001akhil@users.noreply.github.com>
2023-06-18 18:57:25 +0530

    deleted cust

--- customer/auth.js
+++ customer/auth.js
@@ -1,1 +1,11 @@
 //otp end point missing
+const db=require('./dbconnection/connector')
+const otp=(sessionid,phone)=>{
+      return new promise((resolve,reject)=>{
+            db(`select * from customer where ph=${phone}`,(err,result)=>{
+                  
+            })
+
+
+      })
+}
