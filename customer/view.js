f530a8a041260232a175a5b1303b6ba0981ad46a
Akhil.A <56401501+2001akhil@users.noreply.github.com>
2023-06-18 18:45:54 +0530

    error cleared

--- customer/view.js
+++ customer/view.js
@@ -1,23 +1,19 @@
-const db=require('./dbconnector/db');
-module.exports={
-
-      view:()=>{
-            return new promise((resolve,reject)=>{
-            db.query(`SELECT * FROM table_name`,(err,result)=>{
-                  if(err){
-                        reject(err);
-                  }
-                  else{
-                        if(result.length>0){
-                        resolve({})
-                        }
-                        else{
-                              reject({})
-                        }
-
-                  }
-            })
-
-            })
-      }
-}
+//const mongoose=require("mongoose");
+var db = require("../dbconnector/connection");
+module.exports = {
+  view: () => {
+    return new promise((resolve, reject) => {
+      db.query(`SELECT * FROM table_name`, (err, result) => {
+        if (err) {
+          reject(err);
+        } else {
+          if (result.length > 0) {
+            resolve({});
+          } else {
+            reject({});
+          }
+        }
+      });
+    });
+  },
+};
